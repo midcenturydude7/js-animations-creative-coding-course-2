9b4c1ac90d055c2ea9798da54529f271a179e3a9
midCenturyDude7 <mgriffes@gmail.com>
2025-02-01 11:02:31 -0800

    Initial commit | Project structure/org

---
+++ index.js
@@ -0,0 +1,4 @@
+/*
+Getting started with Canvas Sketch
+Canvas Sketch is a free, open-source sketchbook for generative artists and creative coders. It's a desktop app that allows you to create generative art using JavaScript and export high-resolution images and animations.
+*/
