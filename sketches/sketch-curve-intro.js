38d5ca187f0ce2148dd9f4978f31f691e1da36ab
midCenturyDude7 <mgriffes@gmail.com>
2025-02-04 20:37:01 -0800

    Working with event listeners to manipulate points and curves on the canvas

--- sketches/sketch-curve-intro.js
+++ sketches/sketch-curve-intro.js
@@ -2,12 +2,14 @@ const canvasSketch = require("canvas-sketch");
 
 const settings = {
   dimensions: [1080, 1080],
+  animate: true,
 };
 
 let elCanvas;
+let points;
 
 const sketch = ({ canvas }) => {
-  const points = [
+  points = [
     new Point({ x: 200, y: 540 }),
     new Point({ x: 400, y: 300, control: true }),
     new Point({ x: 880, y: 540 }),
@@ -40,10 +42,25 @@ const sketch = ({ canvas }) => {
 const onMouseDown = (e) => {
   window.addEventListener("mousemove", onMouseMove);
   window.addEventListener("mouseup", onMouseUp);
+
+  const x = Math.floor((e.offsetX / elCanvas.offsetWidth) * elCanvas.width);
+  const y = Math.floor((e.offsetY / elCanvas.offsetHeight) * elCanvas.height);
+
+  points.forEach((point) => {
+    point.isDragging = point.hitTest(x, y);
+  });
 };
 
 const onMouseMove = (e) => {
-  console.log(e.offsetX, e.offsetY);
+  const x = Math.floor((e.offsetX / elCanvas.offsetWidth) * elCanvas.width);
+  const y = Math.floor((e.offsetY / elCanvas.offsetHeight) * elCanvas.height);
+
+  points.forEach((point) => {
+    if (point.isDragging) {
+      point.x = x;
+      point.y = y;
+    }
+  });
 };
 
 const onMouseUp = () => {
@@ -70,5 +87,13 @@ class Point {
     context.fill();
 
     context.restore();
+  }
+
+  hitTest(x, y) {
+    const dx = this.x - x;
+    const dy = this.y - y;
+    const dd = Math.sqrt(dx * dx * dy * dy);
+
+    return dd < 20;
   }
 }
