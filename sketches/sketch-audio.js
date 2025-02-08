69ae1ed35c87c5d4381254eb125206a5a6f95206
midCenturyDude7 <mgriffes@gmail.com>
2025-02-07 22:02:29 -0800

    Setting up audio framework

---
+++ sketches/sketch-audio.js
@@ -0,0 +1,28 @@
+const canvasSketch = require("canvas-sketch");
+
+const settings = {
+  dimensions: [1080, 1080],
+};
+
+let audio;
+
+const sketch = () => {
+  audio = document.createElement("audio");
+  audio.src = "sketches/audio/big-city-lights.mp3";
+
+  return ({ context, width, height }) => {
+    context.fillStyle = "white";
+    context.fillRect(0, 0, width, height);
+  };
+};
+
+const addListeners = () => {
+  window.addEventListener("mouseup", () => {
+    if (audio.paused) audio.play();
+    else audio.pause();
+  });
+};
+
+addListeners();
+
+canvasSketch(sketch, settings);
