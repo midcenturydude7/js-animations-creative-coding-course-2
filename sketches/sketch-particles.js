73691c4afc721301692e46dde4801a6909420352
midCenturyDude7 <mgriffes@gmail.com>
2025-02-08 22:56:51 -0800

    Working with image data and colors

--- sketches/sketch-particles.js
+++ sketches/sketch-particles.js
@@ -18,14 +18,25 @@ const colors = colormap({
 });
 
 let elCanvas;
+let imgA;
 
 const sketch = ({ canvas, width, height }) => {
   let x, y, particle, radius;
-  let pos = [];
+  // let pos = [];
 
-  const numCircles = 15;
-  const gapCircle = 8;
-  const gapDot = 4;
+  const imgACanvas = document.createElement("canvas");
+  const imgAContext = imgACanvas.getContext("2d");
+
+  imgACanvas.width = imgA.width;
+  imgACanvas.height = imgA.height;
+
+  imgAContext.drawImage(imgA, 0, 0);
+
+  const imgAData = imgAContext.getImageData(0, 0, imgA.width, imgA.height).data;
+
+  const numCircles = 30;
+  const gapCircle = 2;
+  const gapDot = 2;
   let dotRadius = 12;
   let circRadius = 0;
   const fitRadius = dotRadius;
@@ -37,6 +48,7 @@ const sketch = ({ canvas, width, height }) => {
     const circumference = Math.PI * 2 * circRadius;
     const numFit = i ? Math.floor(circumference / (fitRadius * 2 + gapDot)) : 1;
     const fitSlice = (Math.PI * 2) / numFit;
+    let ix, iy, idx, r, g, b, colA;
 
     for (let j = 0; j < numFit; j++) {
       const theta = fitSlice * j;
@@ -47,9 +59,19 @@ const sketch = ({ canvas, width, height }) => {
       x += width * 0.5;
       y += height * 0.5;
 
-      radius = dotRadius;
+      ix = Math.floor((x / width) * imgA.width);
+      iy = Math.floor((y / height) * imgA.height);
+      idx = (iy * imgA.width + ix) * 4;
 
-      particle = new Particle({ x, y, radius });
+      r = imgAData[idx + 0];
+      g = imgAData[idx + 1];
+      b = imgAData[idx + 2];
+      colA = `rgb(${r}, ${g}, ${b})`;
+
+      // radius = dotRadius;
+      radius = math.mapRange(r, 0, 255, 1, 12);
+
+      particle = new Particle({ x, y, radius, colA });
       particles.push(particle);
     }
 
@@ -60,6 +82,8 @@ const sketch = ({ canvas, width, height }) => {
   return ({ context, width, height }) => {
     context.fillStyle = "black";
     context.fillRect(0, 0, width, height);
+
+    context.drawImage(imgACanvas, 0, 0);
 
     particles.sort((a, b) => a.scale - b.scale);
 
@@ -93,10 +117,25 @@ const onMouseUp = () => {
   cursor.y = 9999;
 };
 
-canvasSketch(sketch, settings);
+const loadImage = async (url) => {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.onload = () => resolve(img);
+    img.onerror = () => reject();
+    img.crossOrigin = "anonymous";
+    img.src = url;
+  });
+};
+
+const start = async () => {
+  imgA = await loadImage("sketches/images/woman-face.jpg");
+  canvasSketch(sketch, settings);
+};
+
+start();
 
 class Particle {
-  constructor({ x, y, radius = 10 }) {
+  constructor({ x, y, radius = 10, colA }) {
     // position
     this.x = x;
     this.y = y;
@@ -115,7 +154,7 @@ class Particle {
 
     this.radius = radius;
     this.scale = 1;
-    this.color = colors[0];
+    this.color = colA;
 
     this.minDist = random.range(100, 200);
     this.pushFactor = random.range(0.01, 0.02);
@@ -137,10 +176,10 @@ class Particle {
 
     this.scale = math.mapRange(dd, 0, 200, 1, 5);
 
-    idxColor = Math.floor(
-      math.mapRange(dd, 0, 200, 0, colors.length - 1, true)
-    );
-    this.color = colors[idxColor];
+    // idxColor = Math.floor(
+    //   math.mapRange(dd, 0, 200, 0, colors.length - 1, true)
+    // );
+    // this.color = colors[idxColor];
 
     // push force
     dx = this.x - cursor.x;
